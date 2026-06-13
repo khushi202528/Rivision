1286d2333fb1b3cf341a80ee9a762ce767d58628
khushi kumari <khhushikumari6@gmail.com>
2026-06-14 00:05:15 +0530

    Revision

---
+++ 2darray/5by5Print10.c
@@ -0,0 +1,12 @@
+// 5 by 5 bol banan hai and print 10 times in c language
+
+#include <stdio.h>
+int main(){
+    int arr[5][5];
+    for(int i = 0;i<5;i++){
+        for(int j =0;j<5;j++){
+            printf("10 ",arr[5][5]);
+        }
+        printf("\n");
+    }
+}
